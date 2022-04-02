ab543d00c64ff8ddf15a195b365a59d8a2259ff4
simidawalang <simidawalang@gmail.com>
2022-04-02 10:34:00 +0100

    added comments to HelloWorld

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.9;
 
 contract HelloWorld {
-    uint number;
+    uint number; // declares a uint256 integer
 
     function storeNumber (uint _num) public {
+        /* "_num" is a temporary variable (disappears after the function call) 
+        used to set "number". */
         number = _num;
     }
 
+    // public function that only returns the value of "number"
     function retrieveNumber() public view returns (uint) {
         return number;
     }
